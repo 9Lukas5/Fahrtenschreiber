920e28edd1cf5a44881ec603e67094e937aa4231
9Lukas5 <Lukas.Wiest@kies-gmbh.de>
2016-11-02 13:12:54 +0100

    initialize Repo

---
+++ src/de/hft_stuttgart/wilu1bif/Fahrtenschreiber.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hft_stuttgart.wilu1bif;
+
+/**
+ * 
+ * HFT-Stuttgart, Semester 1+, Hausaufgabe 4
+ * Fahrtenschreiber programmieren
+ * @author Lukas Wiest
+ */
+public class Fahrtenschreiber
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
